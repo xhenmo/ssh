898e53d0907bbd196169c047e8ea7cad02960958
Xhen Mo <68273411+xhenmo@users.noreply.github.com>
2023-10-15 02:03:48 +0000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-echo root:fujinqi080720 | sudo chpasswd
+echo root:aa112233 | sudo chpasswd
 sudo service ssh restart
